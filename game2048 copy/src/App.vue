c1c67674b549a6cdfd8fc408ce6d73613e5a38fc
zameer chariwala <zameerchariwala@zameers-MacBook-Air.local>
2020-07-02 00:07:45 -0400

    Game2048

---
+++ game2048 copy/src/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <div id="app">
+    <game2048 v-bind:squares="squares" v-on:right-move="rightmove()" v-on:left-move="leftmove()" v-on:up-move="upmove()" v-on:down-move="downmove()"/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  </div>
+</template>
+
+<script>
+
+import game2048 from './components/game2048'
+
+export default {
+  name: 'App',
+  components: {
+    game2048
+  },
+  data(){
+    return {
+      squares:[
+        {
+          id:1,
+          value:1
+        },
+        {
+          id:2,
+          value:1
+        },
+          {
+          id:3
+        },
+        {
+          id:4
+        },
+          {
+          id:5
+        },
+        {
+          id:6
+        },
+          {
+          id:7
+        },
+        {
+          id:8
+        },
+          {
+          id:9
+        },
+        {
+          id:10
+        },
+          {
+          id:11
+        },
+        {
+          id:12
+        },
+          {
+          id:13
+        },
+        {
+          id:14
+        },
+          {
+          id:15
+        },
+        {
+          id:16
+        }
+      ]
+    }
+  },
+  methods:{
+    rightmove(){
+      alert("Right");
+      if(this.squares[0].value === this.squares[1].value)
+      {
+        this.squares[1].value = this.squares[0].value + this.squares[1].value;
+      }
+      alert(this.squares[1].value);
+      
+    },
+    leftmove(){
+      alert("Left");
+      if(this.squares[0].value === this.squares[1].value)
+      {
+        this.squares[0].value = this.squares[0].value + this.squares[1].value;
+      }
+      alert(this.squares[1].value);
+      
+    },
+    upmove(){
+      alert("Up");
+      if(this.squares[0].value === this.squares[1].value)
+      {
+        this.squares[0].value = this.squares[0].value + this.squares[1].value;
+      }
+      alert(this.squares[1].value);
+      
+    },
+    downmove(){
+      alert("Down");
+      if(this.squares[0].value === this.squares[1].value)
+      {
+        this.squares[0].value = this.squares[0].value + this.squares[1].value;
+      }
+      alert(this.squares[1].value);
+      
+    }
+  }
+}
+</script>
+
+
---
+++ game2048 copy/src/components/game2048.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="main-container">
+      <h1><span>Game</span> <span>2</span><span>0</span><span>4</span><span>8</span></h1>
+      <div class="game-area">
+          <div class="game-square" v-bind:key="square.id" v-for="(square) in squares">{{ square.value }}</div>
+          
+      </div>
+      <button v-on:click="$emit('right-move')">Right</button>
+      <button v-on:click="$emit('left-move')">Left</button>
+      <button v-on:click="$emit('up-move')">Up</button>
+      <button v-on:click="$emit('down-move')">Down</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'game2048',
+  props: ["squares"]
+}
+
+</script>
+
+<style scoped>
+    h1{
+        text-align: center;
+    }
+    h1 span:nth-child(1){
+        color:rgb(150, 81, 22);
+    }
+    h1 span:nth-child(2){
+        color: green;
+    }
+    h1 span:nth-child(3){
+        color:turquoise;
+    }
+    h1 span:nth-child(4){
+        color:orangered;
+    }
+    h1 span:nth-child(5){
+        color:orange;
+    }
+    .main-container{
+        text-align: -webkit-center;
+        
+    }
+    .game-area{
+        text-align: center;
+        position: relative;
+        width: 500px;
+        height: 500px;
+        background-color:blue;
+        display: flex;
+        flex-wrap: wrap;
+        border-radius: 10px;
+    }
+    .game-square{
+        width: 21%;
+        height: 21%;
+        margin: 10px;
+        border-radius: 10px;;
+        background-color: chocolate;
+        box-sizing: border-box;
+        justify-content: center;
+        font-size: 6em;
+        font-weight: bold;
+        color: white;
+    }
+</style>
